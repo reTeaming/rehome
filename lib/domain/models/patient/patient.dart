804e8dc0717dd08be0a83f73e988f92ed4b8e34d
LenaHaggi <131677793+LenaHaggi@users.noreply.github.com>
2023-06-03 09:49:14 +0200

    Adding homework to patient.dart

--- lib/domain/models/patient/patient.dart
+++ lib/domain/models/patient/patient.dart
@@ -3,6 +3,7 @@ import 'package:ReHome/domain/models/patient/exerciseDefaultData.dart';
 import 'package:ReHome/domain/models/patient/goals.dart';
 import 'package:ReHome/domain/models/patient/sex.dart';
 import 'package:ReHome/domain/models/user/name.dart';
+import 'package:ReHome/domain/models/user/homework.dart';
 
 class Patient {
   Name name;
@@ -11,7 +12,8 @@ class Patient {
   List<ExerciseDefaultData> exerciseDefaults;
   ClinicalData clinicalData;
   Goals goals;
+  Homework homework;
 
   Patient(this.name, this.sex, this.birthDate, this.exerciseDefaults,
-      this.clinicalData, this.goals);
+      this.clinicalData, this.goals, this.homework);
 }
