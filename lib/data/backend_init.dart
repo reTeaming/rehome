b2b45cc8e78893f775ae26778bf17ee4c56e0de7
Benedict <benedict.roedel@web.de>
2023-06-15 23:08:21 +0200

    API keys für backend wurden in separate Datei gespeichert und durch .gitignore verborgen

--- lib/data/backend_init.dart
+++ lib/data/backend_init.dart
@@ -1,11 +1,10 @@
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import "backend_keys.dart";
 
 // Initialisierung der Backend Anbindung
 class InitializerBackend {
   // Parser wird mit keys (aus back4App) initialisiert
   void initParser() async {
-    const keyApplicationId = 'fF5STvuTrCohAreeYgdO0r1QIsHyTooszAhEwyQe';
-    const keyClientKey = 'aJZGZx48ef4yeF0q8oFG3liiLXfY5yvXFRSkpl44';
     const keyParseServerUrl = 'https://parseapi.back4app.com';
 
     await Parse().initialize(keyApplicationId, keyParseServerUrl,
