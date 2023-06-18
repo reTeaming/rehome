a61b6448d73b2d1075a2dcf8786c362ebe1f27e6
Benedict <116188891+Borajiin@users.noreply.github.com>
2023-06-18 16:56:14 +0200

    Backendanbindung Login
    * backend-gebundene logIn und signUp funktionen wurde erstellt
    
    * Formatierung des Benutzernamens vor backend-Authentifizierung wurde hinzugefügt
    
    * authUser Funktion (backend) gibt nun ParseUser (oder null) zurück
    
    * createUser hat nun einen (optionalen) Email Parameter zum setzen einer Email
    
    * versichern der Initialisierung (ensureInitialized) wird nur in main aufgerufen
    
    * API keys für backend wurden in separate Datei gespeichert und durch .gitignore verborgen

---
+++ lib/data/backend_login.dart
@@ -0,0 +1,31 @@
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+class UserAuth {
+  // Authentifizierung von Endbenutzern durch Backend
+  // User wird anhand vom gegebenem Nutzernamen und Passwort authentifiziert
+  // Rückgabewert ist der nun eingeloggte Benutzer oder null
+  Future<ParseUser?> authUser(String username, String password) async {
+    final user = ParseUser(username.trim(), password, null);
+
+    var response = await user.login();
+    if (response.success) return user;
+    return null;
+  }
+
+  // Registrierung von Endbenutzern (NUR für Entwicklungszwecke)
+  // Benutzer wird mit gegebenem Nutzernamen und Passwort einer generierten Email-Adresse (anhand von Nutzernamen) erstellt
+  Future<bool> createUser(String username, String password,
+      [String? email]) async {
+    final usernameTrimmed = username.trim();
+    final passwordTrimmed = password.trim();
+    final emailTrimmed =
+        (email == null) ? "$usernameTrimmed@gmail.com".trim() : email.trim();
+
+    final user =
+        ParseUser.createUser(usernameTrimmed, passwordTrimmed, emailTrimmed);
+
+    var response = await user.signUp();
+
+    return response.success;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:ReHome/app.dart';
+import 'package:ReHome/data/backend_init.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  // aufruf der parser initialisierung für backend
+  WidgetsFlutterBinding.ensureInitialized();
+  InitializerBackend().initParser();
   // startet die Flutter App
   runApp(const App());
 }
