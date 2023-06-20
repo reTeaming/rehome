17bbe2da2abefbe6c625dca50791bbcc849c8fef
felixmattersberger <felix@mattersberger.de>
2023-06-20 17:42:09 +0200

    theme.dart hinzugefügt, basic ThemeData implementiert

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData rehomeTheme = ThemeData(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Color(0xFF464667),
+    primaryColor: Color(0xFF685BFF),
+    canvasColor: Color(0xFF2E2E48),
+    dividerColor: Color.fromRGBO(255, 255, 255, 0.3),
+    iconTheme: IconThemeData(
+      color: Colors.red.withOpacity(0.7),
+      size: 20,
+    ));
