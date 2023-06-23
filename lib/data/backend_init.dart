efecaa3d267a94faf7299ef782c79532fc2dc8a1
Leon Fuß <leon.fuss@icloud.com>
2023-06-23 16:46:00 +0200

    added .env secret keys

--- lib/data/backend_init.dart
+++ lib/data/backend_init.dart
@@ -1,13 +1,12 @@
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
-import "backend_keys.dart";
 
 // Initialisierung der Backend Anbindung
 class InitializerBackend {
   // Parser wird mit keys (aus back4App) initialisiert
-  void initParser() async {
+  void initParser(String clientKey, String applicationId) async {
     const keyParseServerUrl = 'https://parseapi.back4app.com';
 
-    await Parse().initialize(keyApplicationId, keyParseServerUrl,
-        clientKey: keyClientKey, autoSendSessionId: true);
+    await Parse().initialize(applicationId, keyParseServerUrl,
+        clientKey: clientKey, autoSendSessionId: true);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:ReHome/app.dart';
 import 'package:ReHome/data/backend_init.dart';
+import 'package:ReHome/domain/repositories/secrets_repository.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  // initalize backend
+  var secrets = await SecretsRepositroy.loadFromEnv();
+  if (secrets == null) {
+    throw "Environment Variables for SecrestsRepository not found!";
+  }
+
   // aufruf der parser initialisierung für backend
   WidgetsFlutterBinding.ensureInitialized();
-  InitializerBackend().initParser();
+  InitializerBackend().initParser(secrets.clientKey, secrets.applicationId);
   // startet die Flutter App
-  runApp(const App());
+  runApp(App(secrets));
 }
