328b5bb20009e871f0a8523fe1e3a0ca1f1ce48d
Benedict <116188891+Borajiin@users.noreply.github.com>
2023-06-25 19:10:07 +0200

    Backend - Möglichkeit der User aktualisierung hinzugefügt
    * Funktionen zum Aktualisieren von Nuterdatem im Backend wurden erstellt
    
    * Funktionen zum Aktualisieren der Nutzerdaten im user_repository wurden hinzugefügt
    
    * ParseObject ruft nun Klasse _User und nicht User auf
    
    * Formatierung von ifs in user_repository update Methoden

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -4,6 +4,8 @@ import 'package:ReHome/domain/models/user/id.dart';
 import 'package:ReHome/domain/models/user/institution.dart';
 import 'package:ReHome/domain/models/user/name.dart';
 import 'package:ReHome/domain/models/user/user.dart';
+import 'package:ReHome/data/backend_user.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
 import 'package:uuid/uuid.dart';
 
@@ -12,6 +14,42 @@ import 'package:uuid/uuid.dart';
 // übernommen
 class UserRepository {
   User? _user;
+
+  // aktualisiert Namen des Benutzers und ruft Funktion für Backend Änderung auf
+  void updateName(Name name) async {
+    if (_user == null) return;
+    // Rufe Backend Änderung auf
+    ParseResponse response =
+        await UserBackend().updateBackendName(_user!.id, name);
+    // falls Backend Änderung erfolgreich war, aktualisiere auch Nutzer in Repository
+    if (response.success) {
+      _user = User(_user!.id, name, _user!.username, _user!.institution);
+    }
+  }
+
+  // aktualisiert Namen des Benutzers und ruft Funktion für Backend Änderung auf
+  void updateUsername(Username username) async {
+    if (_user == null) return;
+    // Rufe Backend Änderung auf
+    ParseResponse response =
+        await UserBackend().updateBackendUsername(_user!.id, username);
+    // falls Backend Änderung erfolgreich war, aktualisiere auch Nutzer in Repository
+    if (response.success) {
+      _user = User(_user!.id, _user!.name, username, _user!.institution);
+    }
+  }
+
+  // aktualisiert Namen des Benutzers und ruft Funktion für Backend Änderung auf
+  void updateInstitution(Institution institution) async {
+    if (_user == null) return;
+    // Rufe Backend Änderung auf
+    ParseResponse response =
+        await UserBackend().updateBackendInstitution(_user!.id, institution);
+    // falls Backend Änderung erfolgreich war, aktualisiere auch Nutzer in Repository
+    if (response.success) {
+      _user = User(_user!.id, _user!.name, _user!.username, institution);
+    }
+  }
 
   Future<User?> getUser() async {
     if (_user != null) return _user;
