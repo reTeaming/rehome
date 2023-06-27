2b0512f4932e893836f1e5c4a0660262f0e64fc0
ben <898110054@qq.com>
2023-06-27 20:12:01 +0200

    Settings page with logout function(resubmit)

--- lib/presentation/settings.dart
+++ lib/presentation/settings.dart
@@ -1,12 +1,152 @@
+import 'package:ReHome/business_logic/auth/auth_bloc.dart';
+
+import 'package:ReHome/presentation/login/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Settings")),
+    return  Scaffold(
+      body: Center( 
+        child: Column(
+          children: [
+            const SizedBox(height: 50,),
+
+            const Icon(Icons.account_circle,size: 100,),
+
+            const Text(
+              'Profildaten',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+              ),
+
+            const SizedBox(
+              height: 30,
+              width: 220,
+              child: Divider(
+                color: Colors.black,
+              ),
+            ),
+
+            
+            // Vor- und Nachnamen anzeigen.
+            Builder(
+              builder: (context) {
+                final userName = context.select(
+                  (AuthBloc bloc) => bloc.state.user.name,
+                );
+                return SizedBox(
+                  height: 50,
+                  width: 400,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 239, 239, 239), 
+                    ),
+                    onPressed: null,
+                    child: Row(
+                      children: [
+                        const Icon(Icons.email,color: Colors.black,),
+                        const SizedBox(width: 20,),
+                        Expanded(
+                          child: Text('Name: ${userName.name} ${userName.surname}' ,
+                            style: const TextStyle(fontSize: 15,color: Colors.black),)),
+                        const Icon(Icons.edit,)
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          
+            const SizedBox(height: 10,),
+
+            // Benutzername anzeigen.
+            Builder(
+              builder: (context) {
+                final userName = context.select(
+                  (AuthBloc bloc) => bloc.state.user.username.value,
+                );
+                return SizedBox(
+                  height: 50,
+                  width: 400,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 239, 239, 239), 
+                    ),
+                    onPressed: null,
+                    child: Row(
+                      children: [
+                        const Icon(Icons.email,color: Colors.black,),
+                        const SizedBox(width: 20,),
+                        Expanded(
+                          child: Text('Username: $userName' ,
+                            style: const TextStyle(fontSize: 15,color: Colors.black),)),
+                        const Icon(Icons.edit,)
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+
+            const SizedBox(height: 10,),
+
+            // Institution anzeigen.
+            Builder(
+              builder: (context) {
+                final userIns = context.select(
+                  (AuthBloc bloc) => bloc.state.user.institution,
+                );
+                return SizedBox(
+                  height: 50,
+                  width: 400,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 239, 239, 239), 
+                    ),
+                    onPressed: null,
+                    child: Row(
+                      children: [
+                        const Icon(Icons.email,color: Colors.black,),
+                        const SizedBox(width: 20,),
+                        Expanded(
+                          child: Text('Institution: ${userIns.name}' ,
+                            style: const TextStyle(fontSize: 15,color: Colors.black),)),
+                        const Icon(Icons.edit,)
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+
+            const SizedBox(height: 30,),
+
+            ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+                padding: MaterialStateProperty.all(const EdgeInsets.all(15)),
+                textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 20))),
+              onPressed: () {
+                var bloc = BlocProvider.of<AuthBloc>(context);
+                bloc.add(AuthLogoutRequested());
+                // LoginPage
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                    (_) => false);
+              },
+              child: const Text("Logout",)),
+
+
+        ]),
+      )
     );
   }
 }
