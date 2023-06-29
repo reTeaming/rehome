095a5e766a448ea71d9c0bf970c70ed88a156390
Benedict <benedict.roedel@web.de>
2023-06-29 18:44:50 +0200

    Kommentare zu parseExercise hinzugefügt und parseParameterSet body angepasst

--- lib/data/backend_exercise.dart
+++ lib/data/backend_exercise.dart
@@ -6,24 +6,29 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 class ExerciseBackend {
   // konvertiert gegebenes Exercise Object zu einem ParseObject
   static ParseObject parseExercise(Exercise exercise) {
+    // initialisiert vairablen des Exercise Objekts zur angenehmeren Benutzung
     Id id = exercise.id;
     List<ParameterSet> parameters = exercise.parameter;
     Map<DateTime, List<ParameterSet>> results = exercise.results;
 
+    // Erstellung eines Parse Exercise Objects mit der Id
     ParseObject parseExercise = ParseObject('Exercise')..set('id', id);
 
     List parameterList = List.empty();
 
+    // konvertiere jeden Parameter zu einem ParseObject
     for (var parameter in parameters) {
-      parameterList.add(parseParameterSet(parameter));
+      parameterList.add(parseParameterSet(parameter, results));
     }
 
+    // füge Liste mit Parse Parameter Objekten der Liste hinzu
     parseExercise.set('parameters', parameterList);
 
     return parseExercise;
   }
 
-  static ParseObject parseParameterSet(ParameterSet parameter) {
+  static ParseObject parseParameterSet(
+      ParameterSet parameter, Map<DateTime, List<ParameterSet>> results) {
     return ParseObject('ParameterSet');
   }
 }
