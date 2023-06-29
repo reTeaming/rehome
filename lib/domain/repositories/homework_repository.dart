9155d7689213927c06b25d7ded88b276f45a977e
Benedict <benedict.roedel@web.de>
2023-06-30 01:34:55 +0200

    Ändern der Package namen von ReHome zu rehome im homework repo (nach rebase)

--- lib/domain/repositories/homework_repository.dart
+++ lib/domain/repositories/homework_repository.dart
@@ -1,5 +1,5 @@
-import 'package:ReHome/data/backend_homework.dart';
-import 'package:ReHome/domain/models/patient/homework.dart';
+import 'package:rehome/data/backend_homework.dart';
+import 'package:rehome/domain/models/patient/homework.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
 class HomeworkRepository {
