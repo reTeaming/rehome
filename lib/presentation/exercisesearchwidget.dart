d890e538f235fd2c8dde3090b926d436a539c5ae
Feliks Marksen <felix@atmarxen.de>
2023-07-07 12:28:02 +0200

    Warten auf Merge von #17

---
+++ lib/presentation/exercisesearchwidget.dart
@@ -0,0 +1,109 @@
+//Implementierung von generischer SearchWidget
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../business_logic/search/exercisesearch_bloc.dart';
+import '../business_logic/shared/list/list_bloc.dart';
+
+class ExerciseSearchWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          onChanged: (query) {
+            //Weitergabe der Sucheingabe
+            context.read<ExerciseSearchBloc>().add(SearchInputChanged(query));
+          },
+          decoration: const InputDecoration(
+            hintText: "Suche...",
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(//color),
+                    // Farblich muss das ganze noch angepasst werden
+                    )),
+          ),
+        ),
+        const Divider(height: 10),
+        Flexible(
+          //Filterbuttons eigenem Widget
+          flex: 1,
+          child: BlocBuilder<ExerciseSearchBloc, ListState>(
+            builder: (context, state) {
+              return const Row(children: [
+                FilterButtonWidget("Aktive PatientSearchBlocen"),
+              ]);
+            },
+          ),
+        ),
+        const Divider(height: 10),
+        Flexible(
+          //Anzeige der Patienten als Liste
+          flex: 7,
+          child: BlocBuilder<ExerciseSearchBloc, ListState>(
+              builder: (context, state) {
+            return RefreshIndicator(
+              //key :
+              color: Colors.white,
+              backgroundColor: Colors.blue,
+              strokeWidth: 4.0,
+              onRefresh: () async {
+                //Logik hinter dem Refreshen
+                return Future<void>.delayed(const Duration(milliseconds: 300));
+              },
+              child: ListView(
+                children: <Widget>[
+                  // Erstellen der einzelnen Listenelemente :
+                  for (int index = 0;
+                      index <
+                          state
+                              .list.length; //Länge von der Liste vom Repository
+                      index += 1)
+                    ListTile(
+                      key: Key('$index'),
+                      tileColor: Theme.of(context).focusColor, //random Farbe
+                      title: Text(state.list[index].name.toString()),
+                      onTap: () {
+                        // Hier darf die Weiterleitung zum ExerciseSearchBloc rein
+                      },
+                    ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+
+    //);
+  }
+}
+
+//Eigenes Widget für die Filterbuttons
+class FilterButtonWidget extends StatelessWidget {
+  final String displayText;
+  const FilterButtonWidget(
+    this.displayText, {
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ExerciseSearchBloc, ListState>(
+      builder: (context, state) {
+        return InkWell(
+          onTap: () {
+            //Wenn der Button gedrückt wird ändert sich der Status zum entgegengesetzten
+            context.read<ExerciseSearchBloc>().add(SearchTagChanged(state.tag));
+          },
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(10.0), // Rounded corners
+            ),
+            child: Text(displayText),
+          ),
+        );
+      },
+    );
+  }
+}
