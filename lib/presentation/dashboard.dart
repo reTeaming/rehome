c712b0b7ff7e0bc2d209655bcc5a49ebe28a634f
Leon Fuß <leon.fuss@icloud.com>
2023-07-09 12:12:27 +0200

    moved patient list to patient side

--- lib/presentation/dashboard.dart
+++ lib/presentation/dashboard.dart
@@ -1,16 +1,11 @@
 // Erster Entwurf vom Dashboard (erstmal hauptsächlich um das Scrollbar Widget zu testen)
 import 'package:flutter/material.dart';
-import 'package:rehome/presentation/patientsearchwidget.dart';
 
 class DashBoard extends StatelessWidget {
   const DashBoard({super.key});
 
-  static Route<void> route() {
-    return MaterialPageRoute<void>(builder: (_) => const DashBoard());
-  }
-
   @override
   Widget build(BuildContext context) {
-    return const PatientSearchWidget();
+    return const Text("Dashboard");
   }
 }
