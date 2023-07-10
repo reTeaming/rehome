d871d9981a7eccb5902d65cdfed5fe8a602b4b68
felixmattersberger <felix@mattersberger.de>
2023-07-10 15:07:55 +0200

    Beschreibung der Statistiken hinzugefügt

--- lib/presentation/dashboard.dart
+++ lib/presentation/dashboard.dart
@@ -32,45 +32,45 @@ class DashBoard extends StatelessWidget {
               children: <Widget>[
                 Container(
                   decoration: const BoxDecoration(
-                      color: Colors.blue,
+                      color: myCol,
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   padding: const EdgeInsets.all(8),
-                  child: const Text("Statistik 1"),
+                  child: const Text(statistik1),
                 ),
                 Container(
                   decoration: const BoxDecoration(
-                      color: Colors.blue,
+                      color: myCol,
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   padding: const EdgeInsets.all(8),
-                  child: const Text('Statistik 2'),
+                  child: const Text(statistik2),
                 ),
                 Container(
                   decoration: const BoxDecoration(
-                      color: Colors.blue,
+                      color: myCol,
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   padding: const EdgeInsets.all(8),
-                  child: const Text('Statistik 3'),
+                  child: const Text(statistik3),
                 ),
                 Container(
                   decoration: const BoxDecoration(
-                      color: Colors.blue,
+                      color: myCol,
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   padding: const EdgeInsets.all(8),
-                  child: const Text('Statistik 4'),
+                  child: const Text(statistik4),
                 ),
                 Container(
                   decoration: const BoxDecoration(
-                      color: Colors.blue,
+                      color: myCol,
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   padding: const EdgeInsets.all(8),
-                  child: const Text('Statistik 5'),
+                  child: const Text(statistik5),
                 ),
                 Container(
                   decoration: const BoxDecoration(
-                      color: Colors.blue,
+                      color: myCol,
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   padding: const EdgeInsets.all(8),
-                  child: const Text('Statistik 6'),
+                  child: const Text(statistik6),
                 ),
               ],
             ),
@@ -80,3 +80,17 @@ class DashBoard extends StatelessWidget {
     );
   }
 }
+
+const myCol = Color.fromARGB(255, 221, 240, 255);
+const statistik1 =
+    "Statistik 1: \n Anzahl aktiver Patienten, Anzahl Patienten Archiv \n benötigte Daten: Anbindung Patienten Backend";
+const statistik2 =
+    "Statistik 2: \n durchschnittlich absolvierte Trainingszeit in dieser Woche, evtl. Trendansicht über einen Monat, die am häufigsten durchgeführte Übung \n benötigte Daten: Übungsdaten Backend";
+const statistik3 =
+    "Statistik 3: \n TODOs meiner Patienten \n benötigte Daten: Übungen Backend";
+const statistik4 =
+    "Statistik 4: \n Übung mit der höchsten Erfolgsquote, Übung mit der schlechtesten Erfolgsquote \n benötigte Daten: Übungen Backend";
+const statistik5 =
+    "Statistik 5: \n die Zuletzt absolvierten Übungen mit zugehörigem Patientenname \n benötigte Daten: Übungen Backend, Patientendaten Backend";
+const statistik6 =
+    "Statistik 6: \n Auszeichnungen: Patient X hat alle Übungen für diese Woche abgeschlossen \n benötigte Daten: Übungen Backend, Patientendaten Backend";
