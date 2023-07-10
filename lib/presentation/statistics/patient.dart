b39752a72073453a111bd57805f588f3a4ab6d96
LenaHaggi <lenahaggenmueller@gmail.com>
2023-07-10 14:42:11 +0200

    Säulendiagramm für Wochenübungsstunden

--- lib/presentation/statistics/patient.dart
+++ lib/presentation/statistics/patient.dart
@@ -15,14 +15,14 @@ class Diagramm1 extends StatelessWidget {
         body: Center(
             child: SfCircularChart(
       series: <CircularSeries>[
-        RadialBarSeries<ChartData, String>(
+        RadialBarSeries<ChartData1, String>(
             dataSource: homeworkDone,
-            xValueMapper: (ChartData data, _) => data.week,
-            yValueMapper: (ChartData data, _) => data.done,
+            xValueMapper: (ChartData1 data, _) => data.week,
+            yValueMapper: (ChartData1 data, _) => data.done,
             // die Anzahl, die den vollständigen Kreis beschreibt
             maximumValue: 100,
             // Farbe des Kreisbalkens, je nach Fortschritt
-            pointColorMapper: (ChartData data, _) {
+            pointColorMapper: (ChartData1 data, _) {
               if (data.done < 10) {
                 return Colors.red;
               } else if (data.done < 50) {
@@ -42,6 +42,7 @@ class Diagramm1 extends StatelessWidget {
           textAlign: TextAlign.center,
         )),
       ],
+      // Titel des Diagramms
       title: ChartTitle(
         alignment: ChartAlignment.near,
         text: 'Diese Woche bereits absolviert:',
@@ -52,15 +53,75 @@ class Diagramm1 extends StatelessWidget {
 }
 
 // Klasse der benötigten Daten für das Diagramm
-class ChartData {
-  ChartData(this.week, this.done);
+class ChartData1 {
+  ChartData1(this.week, this.done);
   // week beschreibt den Balken, done beinhaltet die Prozentzahl der Blöcke,
   // und beschreibt somit wie lang der Kreisbalken ist
   final String week;
   final int done;
 }
 
-final List<ChartData> homeworkDone = [
+final List<ChartData1> homeworkDone = [
   // hier muss die Anzahl der Prozentzahl absolvierten blöcke rein (in Prozent)
-  ChartData('dieseWoche', 60),
+  ChartData1('dieseWoche', 60),
+];
+
+class Diagramm2 extends StatelessWidget {
+  const Diagramm2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: SfCartesianChart(
+        enableAxisAnimation: true,
+        series: <ChartSeries<ChartData2, String>>[
+          // Renders column chart
+          ColumnSeries<ChartData2, String>(
+              dataSource: hoursSpent,
+              xValueMapper: (ChartData2 data, _) => data.week,
+              yValueMapper: (ChartData2 data, _) => data.hours,
+              color: primaryColor)
+        ],
+        primaryXAxis: CategoryAxis(
+          majorGridLines: const MajorGridLines(width: 0),
+          autoScrollingDelta: 5,
+          autoScrollingMode: AutoScrollingMode.end,
+        ),
+        zoomPanBehavior: ZoomPanBehavior(enablePanning: true),
+        title: ChartTitle(
+          alignment: ChartAlignment.near,
+          text: 'Übungsstunden pro Woche:',
+          textStyle: rehomeTheme.textTheme.titleLarge,
+        ),
+      ),
+    ));
+  }
+}
+
+class ChartData2 {
+  ChartData2(
+    this.week,
+    this.hours,
+  );
+  final String week;
+  final double hours;
+}
+
+final List<ChartData2> hoursSpent = [
+  ChartData2('Woche 1', 5),
+  ChartData2('Woche 2', 6),
+  ChartData2('Woche 3', 3),
+  ChartData2('Woche 4', 4),
+  ChartData2('Woche 5', 6),
+  ChartData2('Woche 1', 5),
+  ChartData2('Woche 2', 6),
+  ChartData2('Woche 3', 3),
+  ChartData2('Woche 4', 4),
+  ChartData2('Woche 10', 6),
+  ChartData2('Woche 11', 5),
+  ChartData2('Woche 12', 6),
+  ChartData2('Woche 13', 3),
+  ChartData2('Woche 14', 4),
+  ChartData2('Woche 15', 6)
 ];
